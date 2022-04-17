6283b5c177947fc41efa7d4f94ec44cbd155dc05
Arun K <arunk140@users.noreply.github.com>
2022-04-17 15:33:42 +1200

    Testing changes for GNOME 42

--- extension.js
+++ extension.js
@@ -59,6 +59,11 @@ class CommandMenuPopup extends PanelMenu.Button {
     
     let editBtn = new PopupMenu.PopupMenuItem('Edit Commands');
     editBtn.connect('activate', () => {
+      // Check if ~/.commands.json exsists (if not create it)
+      let file = Gio.file_new_for_path(GLib.get_home_dir() + '/.commands.json');
+      if (!file.query_exists(null)) {
+        file.replace_contents(JSON.stringify(commands), null, false, 0, null);
+      }
       // Edit ~/.commands.json
       Gio.AppInfo.launch_default_for_uri('file://' + GLib.get_home_dir() + '/.commands.json', null).launch(null, null);
     });
